a496d693912f861c6d3f9831a2fbaa63eb4142f2
kkamal16 <99223758+kkamal16@users.noreply.github.com>
2022-07-27 13:22:56 +1200

    Add files via upload

---
+++ includes/footer.php
@@ -0,0 +1,118 @@
+<html>
+<body>
+
+
+
+<!-- Footer -->
+<footer class="bg-dark text-center text-white">
+  <!-- Grid container -->
+  <div class="container p-4">
+    <!-- Section: Social media -->
+    <section class="mb-4">
+      <!-- Facebook -->
+      <a class="btn btn-outline-light btn-floating m-1" href="https://www.facebook.com/" role="button"
+        ><i class="fab fa-facebook-f"></i></a>
+
+      <!-- Twitter -->
+      <a class="btn btn-outline-light btn-floating m-1" href="https://twitter.com/?lang=en" role="button"
+        ><i class="fab fa-twitter"></i></a>
+
+      <!-- Google -->
+      <a class="btn btn-outline-light btn-floating m-1" href="https://www.google.com/" role="button"
+        ><i class="fab fa-google"></i></a>
+
+      <!-- Instagram -->
+      <a class="btn btn-outline-light btn-floating m-1" href="https://www.instagram.com/" role="button"
+        ><i class="fab fa-instagram"></i></a>
+
+      <!-- Linkedin -->
+      <a class="btn btn-outline-light btn-floating m-1" href="https://nz.linkedin.com/" role="button"
+        ><i class="fab fa-linkedin-in"></i></a>
+
+      <!-- Github -->
+      <a class="btn btn-outline-light btn-floating m-1" href="https://github.com/login" role="button"
+        ><i class="fab fa-github"></i></a>
+    </section>
+    <!-- Section: Social media -->
+
+
+
+    <!-- Section: Text -->
+    <section class="mb-4">
+      <p>
+      © Kamals Kars
+      </p>
+    </section>
+    <!-- Section: Text -->
+
+    <!-- Section: Links -->
+    <section class="">
+      <!--Grid row-->
+      <div class="row">
+        <!--Grid column-->
+        <div class="col-lg-3 col-md-6 mb-4 mb-md-0">
+          <h5 class="text-uppercase">link1</h5>
+
+          <ul class="list-unstyled mb-0">
+            <li>
+              <a href="" class="text-white">Click here</a>
+            </li>
+          </ul>
+        </div>
+        <!--Grid column-->
+
+        <!--Grid column-->
+        <div class="col-lg-3 col-md-6 mb-4 mb-md-0">
+          <h5 class="text-uppercase">link2</h5>
+
+          <ul class="list-unstyled mb-0">
+            <li>
+              <a href="" class="text-white">Click here</a>
+            </li>
+          </ul>
+        </div>
+        <!--Grid column-->
+
+        <!--Grid column-->
+        <div class="col-lg-3 col-md-6 mb-4 mb-md-0">
+          <h5 class="text-uppercase">link3</h5>
+
+          <ul class="list-unstyled mb-0">
+            <li>
+              <a href="" class="text-white">Click here</a>
+            </li>
+          </ul>
+        </div>
+        <!--Grid column-->
+
+        <!--Grid column-->
+        <div class="col-lg-3 col-md-6 mb-4 mb-md-0">
+          <h5 class="text-uppercase">link4</h5>
+
+          <ul class="list-unstyled mb-0">
+            <li>
+              <a href="" class="text-white">Click here</a>
+            </li>
+          </ul>
+        </div>
+        <!--Grid column-->
+      </div>
+      <!--Grid row-->
+    </section>
+    <!-- Section: Links -->
+  </div>
+  <!-- Grid container -->
+
+  <!-- Copyright -->
+  <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
+    © 2022 Copyright:
+    <a class="text-white" href="">KamalsKars.com</a>
+  </div>
+</footer>
+
+
+
+</body>
+</html>
+
+
---
+++ includes/header.php
@@ -0,0 +1,53 @@
+<!doctype html>
+<html lang="en">
+<head>
+
+<!-- required meta tags -->
+<meta charset="utf-8">
+<meta name="keywords" content="Kamals Curry Restaurant, Kamals Curry Wellington">
+<meta name="description" content="Make bookings and look at Menu at Kamals Curry.">
+
+<title><?php echo $page_title ?></title>
+<!-- css link --> 
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+<link href="css/style.css" rel="stylesheet" type="text/css" />
+
+<!-- js link -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+
+<!--- Link to Awesome fonts - External Link -->
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous">
+
+<!-- link to W3 school theme -->
+<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+<link rel="stylesheet" href="https://www.w3schools.com/lib/w3-theme-black.css">
+</head>
+<body>
+
+<!-- navbar -->
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <a class="navbar-brand" href="index.php">Kamals Kars</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNavDropdown">
+    <ul class="navbar-nav">
+      <li class="nav-item">
+        <a class="nav-link" href="marketplace.php">Marketplace</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="signup.php">Sign up</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="login.php">Log in</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="contactus.php">Contact us</a>
+      </li>
+    </ul>
+  </div>
+</nav>
+<!--Navbar-->
+
+
+
